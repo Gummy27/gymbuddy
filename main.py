84fe6048c7365effc53f2ed5d52647976ef76e49
Gummy27 <brimir27@outlook.com>
2020-08-04 18:10:32 +0000

    Something

--- main.py
+++ main.py
@@ -9,41 +9,66 @@ class Exercise_navigator():
     def next(self):
         if self.index + 1 < self.size:
             self.index += 1
+        print(self.index)
 
     def back(self):
         if self.index - 1 > 0:
             self.index -= 1
+        print(self.index)
 
-workout = WorkoutPlan()
+class GUI():
+    def __init__(self, class_win):
+        self.workout = WorkoutPlan()
+        self.index = 0
+        self.size = len(self.workout.days[0])
+
+        self.navigator = Exercise_navigator(len(self.workout.days[0]))
+
+        # Initializing
+        # Frames
+        self.exercise_frame = Frame(class_win)
+        self.button_frames = Frame(class_win)
+
+        # Labels
+        self.exercise_label = Label(self.exercise_frame, text=self.workout.days[0][self.index])
+
+        # Buttons
+        self.back_button = Button(self.button_frames, text="Back", command=self.button_back)
+        self.next_button = Button(self.button_frames, text="Next", command=self.button_next)
+
+
+        # Packing
+        # Frames
+        self.exercise_frame.grid(row=0)
+        self.button_frames.grid(row=1)
+
+        # Labels
+        self.exercise_label.grid(sticky=E)
+
+        # Buttons
+        self.back_button.grid(row=0, column=0, sticky=E)
+        self.next_button.grid(row=0, column=1, sticky=E)
+
+
+    def button_next(self):
+        if self.index + 1 < self.size:
+            self.index += 1
+            self.exercise_label.config(text=self.workout.days[0][self.index])
+
+    def button_back(self):
+        if self.index + 1 > 0:
+            self.index -= 1
+            self.exercise_label.config(text=self.workout.days[0][self.index])
+
+
+
+
+
 
 win = Tk()
 win.geometry("300x500")
 
-banner = Label(win, text="This is a banner")
-banner.pack(fill=Y)
-
-# Valmynd yfir hvaða dag þú ert á í planinu.
-days_frame = Frame(win)
-
-days_buttons = []
-for x in range(len(workout.days)):
-    button = Button(days_frame, text=f"Day {x}")
-    button.pack(fill=Y)
-    days_buttons.append(button)
-
-# Æfingin sem þú ert á
-exercise_frame = Frame(win)
-
-index = 0
-exercise_label = Label(exercise_frame, text=workout.days[0][index])
-exercise_label.pack()
-
-navigator = Exercise_navigator(len(workout.days[0]))
-
-back_button = Button(exercise_frame, text="Back", command=navigator.back)
-back_button.pack()
-
-exercise_frame.pack()
+interface = GUI(win)
 
 win.mainloop()
 
